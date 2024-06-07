4b855cf0a468655ff6f27b068ef84a1344cd6973
Rinor Kastrati <rinor.kastrati@pabau.com>
2024-06-07 10:44:43 +0200

    second scrapper

--- scraper2.py
+++ scraper2.py
@@ -6,13 +6,13 @@ from selenium.webdriver.support import expected_conditions as EC
 import codecs
 from webdriver_manager.chrome import ChromeDriverManager
 import json
-from selenium.webdriver.support.expected_conditions import url_changes
 import time
 from concurrent.futures import ProcessPoolExecutor, as_completed
+from selenium.common.exceptions import NoSuchElementException
 import random
-entries = []    
 
 def scrape_data(url):
+    entries = []
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     wait = WebDriverWait(driver, 10)
 
@@ -27,7 +27,6 @@ def scrape_data(url):
             while True: 
                     time.sleep(random.uniform(1.2, 2))
                     box_items = driver.find_elements(By.CSS_SELECTOR, "cw-box-grid-item-ecommerce.ng-star-inserted")
-
                     if index >= len(box_items):
                         print("No more box items to scrape. Exiting the loop.")
                         break
@@ -37,9 +36,17 @@ def scrape_data(url):
                     wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'name')))
                     wrapperName = item.find_element(By.CSS_SELECTOR, 'a.name').text
                     wrapperCategory = item.find_element(By.CSS_SELECTOR, 'div.category').text
+                    tags = [{"name": tag.strip()} for tag in wrapperCategory.split(',')]
                     wrapperPrice = item.find_element(By.CSS_SELECTOR, 'span.currency-value').text
                     wrapperIconUrl = item.find_element(By.CSS_SELECTOR, 'img').get_attribute('src')
                     link = item.find_element(By.CSS_SELECTOR, 'a.img-container.position-relative.animation')
+                    box = {
+                            "name": wrapperName,
+                            "tags": tags,
+                            "cost": wrapperPrice,
+                            "iconUrl": wrapperIconUrl
+                    }
+                    box_slots = []
                     time.sleep(random.uniform(1.2, 2.2))
                     if link:
                             driver.execute_script("arguments[0].scrollIntoView();", link) 
@@ -55,6 +62,19 @@ def scrape_data(url):
                                                 time.sleep(random.uniform(1.5, 2))
                                                 button = item_element.find_element(By.CSS_SELECTOR, "button.mat-focus-indicator.view-btn.mat-flat-button.mat-button-base")
                                                 driver.execute_script("arguments[0].click();", button)
+                                                read_more = None
+                                                box_description = None
+
+                                                try:
+                                                    read_more = item_element.find_element(By.CSS_SELECTOR, "span.text-primary.pointer.ng-star-inserted")
+                                                    read_more.click()
+                                                except NoSuchElementException:
+                                                    pass
+
+                                                try:
+                                                    box_description = item_element.find_element(By.CSS_SELECTOR, "div.d-inline")
+                                                except NoSuchElementException:
+                                                    pass
                                                 rate = item_element.find_element(By.CSS_SELECTOR, "div.rate").text
                                                 name_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.body h2")))
                                                 brand_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.body div.details")))
@@ -67,35 +87,44 @@ def scrape_data(url):
                                                 brand = brand_element.text
                                                 price = price_element.text
                                                 outcome = outcome_element.text
+                                                min_outcome = outcome.split('-')[0].strip()
+                                                max_outcome = outcome.split('-')[1].strip()
+                                                # type_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".cw-item-rarity[placement='right-bottom'] .cw-icon")))
+                                                # type_class = type_element.get_attribute("class").split('-')[1]
                                                 description = description_element.text
                                                 other_categories = driver.find_elements(By.CSS_SELECTOR, "div.box.cursor-pointer.ng-star-inserted")
-                                                entry = {
-                                                        "Box": {
-                                                            "Name": wrapperName,
-                                                            "Category": wrapperCategory,
-                                                            "Price": wrapperPrice,
-                                                            "Icon URL": wrapperIconUrl
-                                                        },
-                                                        "Name": name,
-                                                        "Brand": brand,
-                                                        "Cost": price,
-                                                        "Rate": rate,
-                                                        "Outcome": outcome,
-                                                        "Image URL": img_src,
-                                                        "Description": description,
-                                                        "Category": []
+                                                box_slot = {
+                                                            "rate": rate,
+                                                            "item": {
+                                                                "name": name,
+                                                                "description": description,
+                                                                "iconUrl": img_src,
+                                                                "brand": brand,
+                                                                "category": wrapperCategory,
+                                                                "price": price,
+                                                                "displayValue": outcome,
+                                                                "minDisplayValue": min_outcome,
+                                                                "maxDisplayValue": max_outcome,
+                                                                'category': []
+                                                                # "type": type_class
+                                                            }
                                                 }
                                                 for category in other_categories:
                                                     category_name = category.find_element(By.CSS_SELECTOR, "div.text.ng-star-inserted").text
-                                                    category_element = category.find_element(By.CSS_SELECTOR, "img")
-                                                    entry["Category"].append({"name": category_name, "image_url": category_element.get_attribute("src")})
-                                                file.write(f"Box: {entry}\n")
-                                                entries.append(entry)
+                                                    box_slot["item"]['category'].append({"name": category_name })
+                                                box_slots.append(box_slot)
+
 
                                                 close_button = driver.find_element(By.CSS_SELECTOR, "button.mat-focus-indicator.close.mat-icon-button.mat-button-base")
                                                 driver.execute_script("arguments[0].click();", close_button)
                                                 time.sleep(random.uniform(1, 2))
-                                                # driver.back()
+                                    
+                                    entry = {
+                                        "Box": box,
+                                        "BoxSlots": box_slots
+                                    }
+                                    file.write(f"Box: {entry}\n")
+                                    entries.append(entry)
                                 time.sleep(2)
                                 driver.back()
                                 wait.until(EC.url_to_be(url))
@@ -103,25 +132,41 @@ def scrape_data(url):
                     index += 1
 
                     print("done")
+                    
+                    js_script = open('loadmore.js', 'r').read()
+                    load_more_clicked = driver.execute_script(js_script)
+                    if not load_more_clicked:
+                        break
 
                     wait = WebDriverWait(driver, 4)
         else:
             print("url bad")
+            return entries
     except Exception as e:
         print("An error occurred:", e)
     finally:
         driver.quit()
+    return entries
 
 if __name__ == "__main__":
-    urls_to_scrape = []
-
+    urls_to_scrape = [
+        "https://www.hypedrop.com/en/boxes/eu/list/",
+        "https://www.hypedrop.com/en/boxes/eu/list/hot",
+        "https://www.hypedrop.com/en/boxes/eu/list/updated",
+        "https://www.hypedrop.com/en/boxes/eu/list/streetwear",
+        "https://www.hypedrop.com/en/boxes/eu/list/tech",
+        "https://www.hypedrop.com/en/boxes/eu/list/gaming"
+    ]
+    
+    all_entries = []
     with ProcessPoolExecutor(max_workers=3) as executor:
         futures = {executor.submit(scrape_data, url): url for url in urls_to_scrape}
 
         for future in as_completed(futures):
             url = futures[future]
             try:
-                future.result()  # get the result of the function
+                result = future.result() 
+                all_entries.extend(result)
             except Exception as exc:
                 print(f"{url} generated an exception: {exc}")
 
@@ -175,5 +220,5 @@ def write_to_json_file(unique_entries, output_file):
     with open(output_file, 'w') as file:
         json.dump(unique_entries, file, indent=4)
 
-unique_entries = remove_duplicates(entries)
+unique_entries = remove_duplicates(all_entries)
 write_to_json_file(unique_entries, "article_scraping.json")
